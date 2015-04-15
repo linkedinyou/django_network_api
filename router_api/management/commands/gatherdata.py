7b9a92fa929a601b26496935306b9b3a866d0f10
mitchm87@gmail.com <mitchm87@gmail.com>
2015-04-14 18:07:06 -0600

    Updated script

--- router_api/management/commands/gatherdata.py
+++ router_api/management/commands/gatherdata.py
@@ -4,13 +4,25 @@ from router_api.models import Router
 from netw0rk import Juniper
 
 class Command(BaseCommand):
-        help = 'Polls a device for all of the information we need in our router API'
+    ''' This management script could be used by cron to schedule 
+        gathering data from all of the devices in our database.
 
-        def handle(self, *args, **options):
-            for router in Router.objects.all():
-                self.gatherData(router.name)
+        Unfortunately, this is just an example as my "desktop" that I
+        am building this on does not have access to the management network to pull
+        router data.
 
-        def gatherData(self, device):
-            device = Juniper(device)
-            for vpn in device.vpns:
-                print vpn
+        I will have to use the API to write data from a bastion host.
+        '''
+    help = 'Polls a device for all of the information we need in our router API'
+
+    def handle(self, *args, **options):
+        for router in Router.objects.all():
+            self.gatherData(router.name)
+
+    def gatherData(self, device):
+        device = Juniper(device)
+        for vpn in device.vpns:
+            print vpn
+            ''' Save each VPN into the database for this device, etc. '''
+
+        ''' write out device.cpu, device.memory, etc '''
