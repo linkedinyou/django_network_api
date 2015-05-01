b8dd1bc0908333b426b418fd5cd1c97b1ed043a4
mitchm87@gmail.com <mitchm87@gmail.com>
2015-05-01 16:43:41 -0600

    Updated models

--- router_api/serializers.py
+++ router_api/serializers.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 from .models import *
 
-class CpuSerializer(serializers.HyperlinkedModelSerializer):
+class CpuSerializer(serializers.ModelSerializer):
     router = serializers.PrimaryKeyRelatedField(queryset=Router.objects.all())
 
     class Meta:
@@ -19,7 +19,7 @@ class MemorySerializer(serializers.ModelSerializer):
         fields = ('id', 'time', 'utilization', 'url', 'router')
 
 
-class RouteCountSerializer(serializers.HyperlinkedModelSerializer):
+class RouteCountSerializer(serializers.ModelSerializer):
     vpn = serializers.PrimaryKeyRelatedField(queryset=Vpn.objects.all())
 
     class Meta:
@@ -27,12 +27,12 @@ class RouteCountSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('id', 'time', 'count', 'url', 'vpn')
 
 
-class VpnSerializer(serializers.HyperlinkedModelSerializer):
-    num_routes = RouteCountSerializer(many = True, read_only = True)
+class VpnSerializer(serializers.ModelSerializer):
+    numroutes = RouteCountSerializer(many = True, read_only = True)
 
     class Meta:
         model = Vpn
-        fields = ('id', 'name', 'url', 'router', 'route_target', 'import_target', 'num_routes')
+        fields = ('id', 'name', 'url', 'router', 'route_target', 'import_target', 'numroutes')
 
 
 class InterfaceSerializer(serializers.ModelSerializer):
@@ -40,7 +40,7 @@ class InterfaceSerializer(serializers.ModelSerializer):
         model = Interface
 
 
-class RouterSerializer(serializers.HyperlinkedModelSerializer):
+class RouterSerializer(serializers.ModelSerializer):
     cpus = CpuSerializer(many = True, read_only = True)
     memorys = MemorySerializer(many = True, read_only = True)
     interfaces = InterfaceSerializer(many = True, read_only = True) 
